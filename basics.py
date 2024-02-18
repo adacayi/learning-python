03a064668d816ced1485236c88f91033c52f09a9
Abdullah Sanver <abdullah.sanver@valtech.com>
2024-02-18 20:53:29 +0000

    Minor refactorings

--- basics.py
+++ basics.py
@@ -6,7 +6,9 @@ x = 3
 y = x - 2
 
 if x != y:
-    print(x, "does not equal", y)  # One line comment. Print puts a space in the output between its parameters and puts a newline at the end
+    print(x, "does not equal to", y)  # One line comment. Print puts a space in the output between its parameters and puts a newline at the end. You can override this behavior by setting sep = "some separator", which can also be set to empty string and you can set end = ""
+    print(x, "does not equal to", y, sep = "-", end = "+++")
+    print(x, "does not equal to", y, sep = "")
 
 name = "Adam"
 surname = "Smith"
